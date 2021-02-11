9db5e922de2bfb4bc0f2b8ae29a336174c63aafc
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-02-11 10:32:45 -0500

    Solving modules problems

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+    return {
+      sayIt: function(){
+        return this.greeting.concat(', ').concat(this.name);
+      },
+      name: str2,
+      greeting: str1
+    };
   }
 };
